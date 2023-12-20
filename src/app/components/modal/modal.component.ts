168a2d29b8b1334ed280a749bd0ef3c35f88bab5
urvesh21 <urvesh21@gmail.com>
2023-12-20 17:10:48 -0500

    Fix modal style issue

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,25 +1,29 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MatLegacyDialogRef as MatDialogRef, MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA, MatLegacyDialogModule } from '@angular/material/legacy-dialog';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Dialog, DialogRef, DIALOG_DATA, DialogModule } from '@angular/cdk/dialog';
 import { Observable } from 'rxjs';
 import { MovieDbService } from 'src/app/services/movie-db.service';
 import { NgIf, NgFor, AsyncPipe } from '@angular/common';
 
 @Component({
-    selector: 'app-modal',
-    templateUrl: './modal.component.html',
-    styleUrls: ['./modal.component.scss'],
-    standalone: true,
-    imports: [NgIf, MatLegacyDialogModule, NgFor, AsyncPipe]
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss'],
+  standalone: true,
+  imports: [NgIf, DialogModule, NgFor, AsyncPipe],
 })
 export class ModalComponent implements OnInit {
   playVideo: boolean = false;
   loadingError$: Observable<string>;
 
-  constructor(public dialogRef: MatDialogRef<ModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any, public sanitizer: DomSanitizer, private movieDb: MovieDbService) {
-      this.loadingError$ = this.movieDb.loadingError.asObservable();
-    }
+  constructor(
+    public dialogRef: DialogRef<ModalComponent>,
+    @Inject(DIALOG_DATA) public data: any,
+    public sanitizer: DomSanitizer,
+    private movieDb: MovieDbService
+  ) {
+    this.loadingError$ = this.movieDb.loadingError.asObservable();
+  }
 
   ngOnInit(): void {
     this.data.video = this.sanitizer.bypassSecurityTrustResourceUrl(this.data.video);
@@ -29,4 +33,7 @@ export class ModalComponent implements OnInit {
     this.playVideo = true;
   }
 
+  closeModal() {
+    this.dialogRef.close();
+  }
 }
